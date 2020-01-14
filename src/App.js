3275067107bb04690c6ddcd5d2082aff97c1e038
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-01-14 02:53:14 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import NoteForm from "./Components/NoteForm";
+import Notes from "./Components/Notes";
+import ReactDOM from "react-dom";
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+console.log('what is going on?');
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "Happy little quote",
+      body:
+        "Talent is a pursued interest. Anything that you're willing to practice, you can do.― Bob Ross "
+    }
+  ]);
+
+  const addNewNote = note => {
+    const newNote = {
+      id: Date.now(),
+      title: note.title,
+      body: note.body
+    };
+    setNotes([...notes, newNote]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Notes</h1>
+      aaaaaaaaaaaaaa
+      {/* we are going to pass a function down as a prop */}
+      <NoteForm addNewNote={addNewNote} />
+      <Notes notes={notes} />
     </div>
   );
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
+
